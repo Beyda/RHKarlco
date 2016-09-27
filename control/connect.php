1f1111c565f1bc4eba59e2c5498278d3b57b91f2
beyda <1130032@unav.edu.mx>
2016-09-26 17:59:23 -0700

    26-09-16

--- control/connect.php
+++ control/connect.php
@@ -1,6 +1,6 @@
 <?php
-//$mysqli = new mysqli("localhost", "admin_rhkarlco", "bdrhk16", "admin_rhkarlco");
-$mysqli = new mysqli("localhost", "root", "", "admin_rhkarlco");
+$mysqli = new mysqli("localhost", "admin_rhkarlco", "bdrhk16", "admin_rhkarlco");
+//$mysqli = new mysqli("localhost", "root", "", "admin_rhkarlco");
 /* check connection */
 if (mysqli_connect_errno()) {
     echo "<li>Coneccion Fallida!! : %s\n", mysqli_connect_error();
