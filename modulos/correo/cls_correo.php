88b414c1cc84b162a0ce9c84f037ee0e6c1dc8fc
beyda <1130032@unav.edu.mx>
2016-10-06 14:22:02 -0700

    06-10-16

--- modulos/correo/cls_correo.php
+++ modulos/correo/cls_correo.php
@@ -65,13 +65,7 @@
 									}</script>";
 									$v = 1;
 						}
-					} else {
-						echo "<script>if(confirm('Este archivo ya existe')){ 
-						document.location='l_correo.php';} 
-						else{ alert('Operacion Cancelada'); 
-						}</script>";
-						$v = 1;
-					}
+					} 
 				} else {
 					echo "<script>if(confirm('Este tipo de archivo no es permitido')){ 
 							document.location='l_correo.php';} 
@@ -105,59 +99,45 @@
 					$correos = "SELECT DISTINCT d.`primer_nombre`, d.`segundo_nombre`, d.`ap_paterno`, d.`ap_materno`, d.`correo` FROM `datos_personales` d INNER JOIN `puesto_per` pp ON d.`id_datosper` = pp.`id_datosper` INNER JOIN `puestos` p ON pp.`id_puesto` = p.`id_puesto` INNER JOIN `empresas` e ON p.`id_empresa` = e.`id_empresa` AND e.`id_empresa` = $this->empresa";
 					$resul_correos = $mysqli->query($correos);
 
-					require '../mail/PHPMailerAutoload.php';
+					require '../mail/class.phpmailer.php';
 
 
-					$mail = new PHPMailer;//Create a new PHPMailer instance
+					$mail = new PHPMailer();
 
-
-					$mail->isSMTP();//Tell PHPMailer to use SMTP
-
-
-					$mail->SMTPDebug = 0;//Enable SMTP debugging
-					// 0 = off (for production use)
-					// 1 = client messages
-					// 2 = client and server messages
-
-
-					$mail->Debugoutput = 'html';//Ask for HTML-friendly debug output
-					$mail->Host = 'smtp.gmail.com';//hostname del servidor de gmail
-					$mail->Port = 465;//puerto del host de gmail
-					$mail->SMTPSecure = 'ssl';//envio de encriptación ssl
-					$mail->SMTPAuth = true;//Whether to use SMTP authentication
-					$mail->Username = "rhkarlco";//usuario creado en gmail de smtp
-					$mail->Password = 'rhkarlco2016';//contraseña del smtp
-					$mail->setFrom('amor.sanchez@karlcogroup.com', 'Recursos Humanos');//correo que envia el correo
-					$mail->addReplyTo('rhkarlco@gmail.com', 'Recursos Humanos');//correo donde se debe responder el correo
-					while ($row_correos = $resul_correos->fetch_array()) 
+				    $mail->From     = 'rhkarlco@gmail.com';
+				    $mail->FromName = 'Rhkarlco'; 
+				    while ($row_correos = $resul_correos->fetch_array()) 
                       	{
-                      		echo "$row_correos[4]";
-					echo $mail->addAddress($row_correos[4] , $row_correos[0] $row_correos[1] $row_correos[2] $row_correos[3]);//correos a donde se envia el mensaje
+					$mail->addAddress(utf8_decode($row_correos[4]) , utf8_decode($row_correos[0]) .' '. utf8_decode($row_correos[1]) .' '. utf8_decode($row_correos[2]) .' '. utf8_decode($row_correos[3]));//correos a donde se envia el mensaje
 						}
 					$mail->Subject = $this->asunto ;//Tema del mensaje
+				   
+				// Aquí van los datos que apareceran en el correo que reciba
+				    //adjuntamos un archivo 
+				        //adjuntamos un archivo
+				            
+				    $mail->Subject  =  "Contacto";
+				    $mail->Body     =  "$this->mensaje \n<br />";
+				    
+				    $mail->AddAttachment('$this->tmp_name', '$this->archivo');
+				    echo "$this->tmp_name, $this->archivo";
+				    
+				    
 
-					//Read an HTML message body from an external file, convert referenced images to embedded,
-					//convert HTML into a basic plain-text alternative body
-					//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-					//$mail->isHTML(true);
-					$mail->msgHTML("
-						<!DOCTYPE html>
-					  	<html>
-					  	<head>
-					  		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-					  	</head>
-					  	<body>
-					  		<p>$this->mensaje </p>
-					  	</body>
-					  	</html>
-						");
-					$name=$this->asunto;
-					$mail->AltBody = $this->asunto;//Replace the plain text body with one created manually
+				// Datos del servidor SMTP
 
-					//Attach an image file
-					//$mail->addAttachment('images/phpmailer_mini.png');
-					//send the message, check for errors
-					if (!$mail->send()) {
+				    $mail->IsSMTP(); 
+				    $mail->Debugoutput = 'html';//Ask for HTML-friendly debug output
+                    $mail->Host = 'smtp.gmail.com';//hostname del servidor de gmail
+                    $mail->Port = 465;//puerto del host de gmail
+                    $mail->SMTPSecure = 'ssl';//envio de encriptación ssl
+                    $mail->SMTPAuth = true;//Whether to use SMTP authentication
+                    $mail->Username = "rhkarlco";//usuario creado en gmail de smtp
+                    $mail->Password = 'rhkarlco2016';//contraseña del smtp
+
+                    echo "hola";
+					
+					if (!$mail->Send()) {
 					    echo "Mailer Error: " . $mail->ErrorInfo;
 					} else {
 					    echo "Message enviado!";
