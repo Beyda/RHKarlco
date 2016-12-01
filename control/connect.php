f95ee227ba0569018d18dc820294f92c1ee9ef75
Beyda <1130032@unav.edu.mx>
2016-12-01 13:19:53 -0700

    Update connect.php

--- control/connect.php
+++ control/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "admin_rhkarlco", "bdrhk16", "admin_rhkarlco");
+$mysqli = new mysqli("localhost", "", "", "");
 //$mysqli = new mysqli("localhost", "root", "", "admin_rhkarlco");
 /* check connection */
 if (mysqli_connect_errno()) {
